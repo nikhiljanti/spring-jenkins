fb158df010530cb7d397338ea8f9e921b57c2ec7
nikhiljanti <nikhil.janti@gmail.com>
2022-06-18 15:59:53 +0530

    First commit

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/index")
+	public String showData() {
+		return "Welcome to Spring - Jenkins...!";
+	}
+
+}
